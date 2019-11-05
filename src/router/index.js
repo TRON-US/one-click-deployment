a2fdf7dee54cfc4ca5eebd169cd706e698c351d7
liuximing <459794155@qq.com>
2019-11-05 20:27:14 +0800

    permission

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,19 @@ import Layout from '@/newLayout';
  * a base page that does not have permission requirements
  * all roles can be accessed
  */
+// export const constantRoutes = [
+//   {
+//     path: '/redirect',
+//     component: Layout,
+//     hidden: true,
+//     children: [
+//       {
+//         path: '/redirect/:path*',
+//         component: () => import('@/views/redirect/index')
+//       }
+//     ]
+//   },
+// ]
 export const constantRoutes = [
 
   {
