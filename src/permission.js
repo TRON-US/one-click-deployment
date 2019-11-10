9113b2f95b2238ab4fb8fd2607336c3a0a5fbc6b
liuximing <459794155@qq.com>
2019-11-10 11:26:17 +0800

    new build

--- src/permission.js
+++ src/permission.js
@@ -30,61 +30,61 @@ router.beforeEach(async (to, from, next) => {
   const hasToken = getToken()
 
   console.log('token', hasToken)
-  if (hasToken) {
-    if (to.path === '/login') {
-      // if is logged in, redirect to the home page
-      next({
-        path: '/'
-      })
-      NProgress.done()
+  // if (hasToken) {
+  if (to.path === '/login') {
+    // if is logged in, redirect to the home page
+    next({
+      path: '/'
+    })
+    NProgress.done()
+  } else {
+    // determine whether the user has obtained his permission roles through getInfo
+    const hasRoles = store.getters.roles && store.getters.roles.length > 0
+    console.log(hasRoles)
+    if (hasRoles) {
+      next()
     } else {
-      // determine whether the user has obtained his permission roles through getInfo
-      const hasRoles = store.getters.roles && store.getters.roles.length > 0
-      console.log(hasRoles)
-      if (hasRoles) {
+      try {
+        // get user info
+        // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
+        const {
+          roles
+        } = await store.dispatch('user/getInfo')
+
+        // generate accessible routes map based on roles
+        const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+
+
+        // dynamically add accessible routes
+        router.addRoutes(accessRoutes)
+
+        // hack method to ensure that addRoutes is complete
+        // set the replace: true, so the navigation will not leave a history record
+        next({
+          ...to,
+          replace: true
+        })
         next()
-      } else {
-        try {
-          // get user info
-          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-          const {
-            roles
-          } = await store.dispatch('user/getInfo')
-
-          // generate accessible routes map based on roles
-          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
-
-
-          // dynamically add accessible routes
-          router.addRoutes(accessRoutes)
-
-          // hack method to ensure that addRoutes is complete
-          // set the replace: true, so the navigation will not leave a history record
-          next({
-            ...to,
-            replace: true
-          })
-          next()
-        } catch (error) {
-          // remove token and go to login page to re-login
-          await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
-          next()
-          NProgress.done()
-        }
+      } catch (error) {
+        // remove token and go to login page to re-login
+        await store.dispatch('user/resetToken')
+        Message.error(error || 'Has Error')
+        next()
+        NProgress.done()
       }
     }
-  } else {
-    /* has no token*/
-    if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
-      next()
-    } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      next()
-      NProgress.done()
-    }
   }
+  // } else {
+  //   /* has no token*/
+  //   if (whiteList.indexOf(to.path) !== -1) {
+  //     // in the free login whitelist, go directly
+  //     next()
+  //   } else {
+  //     // other pages that do not have permission to access are redirected to the login page.
+  //     next()
+  //     NProgress.done()
+  //   }
+  // }
 
 })
 
